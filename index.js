c13bd4ef3acd46e0dd665f14b6cdf6c1302d3f0b
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2020-03-26 15:41:21 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+const {systemPreferences} = require('electron');
+const execa = require('execa');
+
+const readDefaults = async () => {
+  const {stdout} = await execa('defaults', ['read', 'com.apple.systemuiserver', 'menuExtras']);
+  return stdout;
+};
+
+const writeDefaults = async defaults => {
+  await execa('defaults', ['write', 'com.apple.systemuiserver', 'menuExtras', defaults]);
+  await execa('killall', ['SystemUIServer']);
+};
+
+const willStartRecording = async ({state}) => {
+  const defaults = await readDefaults();
+  systemPreferences.postNotification('com.wulkano.kap.hideclock.willStartRecording', {});
+
+  if (defaults.includes('Clock.menu')) {
+    state.defaults = defaults;
+
+    const defaultsWithoutClock = defaults
+      .split('\n')
+      .filter(line => !line.includes('Clock.menu'))
+      .join('\n');
+
+    await writeDefaults(defaultsWithoutClock);
+  }
+};
+
+const didStopRecording = async ({state}) => {
+  systemPreferences.postNotification('com.wulkano.kap.hideclock.didStopRecording', {});
+
+  if (state.defaults) {
+    await writeDefaults(state.defaults);
+  }
+};
+
+const hideClock = {
+  title: 'Hide Clock',
+  willStartRecording,
+  didStopRecording
+};
+
+exports.recordServices = [hideClock];
---
+++ readme.md
@@ -0,0 +1,20 @@
+# kap-hide-clock [![Build Status](https://travis-ci.org/karaggeorge/kap-hide-clock.svg?branch=master)](https://travis-ci.org/karaggeorge/kap-hide-clock)
+
+> [Kap](https://github.com/wulkano/kap) plugin - Hide the clock while recording
+
+
+## Install
+
+In the `Kap` menu, go to `Preferences…`, select the `Plugins` pane, find this plugin, and toggle it.
+
+
+## Usage
+
+In the cropper or by right clicking the tray icon, click the `…` icon, then `Plugins` and make sure `Hide Clock` is enabled.
+
+## Integration
+
+If you would like your date/time app to integrate with this, you can use the two notifications posted by the plugin:
+
+- `com.wulkano.kap.hideclock.willStartRecording`
+- `com.wulkano.kap.hideclock.didStopRecording`
